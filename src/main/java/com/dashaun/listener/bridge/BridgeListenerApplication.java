ef865f2c151dc64ef7d755218ac1bac7a64640a0
DaShaun <dashaun@redislabs.com>
2021-07-31 09:42:54 -0500

    start.spring.io

---
+++ src/main/java/com/dashaun/listener/bridge/BridgeListenerApplication.java
@@ -0,0 +1,13 @@
+package com.dashaun.listener.bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgeListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BridgeListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dashaun/listener/bridge/BridgeListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashaun.listener.bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgeListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
